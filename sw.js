cedec6bd2210640bc188516f7eba485a81ebd8e5
Carol Camilo <carolcamilobranding@gmail.com>
2026-03-25 00:51:42 -0300

    fix: login overlay with z-index + cache bust v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maricota-cadastro-v1';
+const CACHE_NAME = 'maricota-cadastro-v2';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -17,10 +17,10 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    fetch(e.request).then(res => {
       const clone = res.clone();
       caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
       return res;
-    }))
+    }).catch(() => caches.match(e.request))
   );
 });
